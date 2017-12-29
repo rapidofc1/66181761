12fcd165fe417a61de9927f1bb054eaeafb6db3a
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-12-29 11:21:54 -0700

    Create core.py

---
+++ cogs/core.py
@@ -0,0 +1,24 @@
+import openweathermapy.core as weather
+import unicodedata
+import youtube_dl
+import pyowm
+import traceback
+import os
+import sys
+import sqlite3
+import discord
+from discord.ext import commands
+import random
+import asyncio
+from datetime import datetime
+import aiohttp
+import json
+import requests
+import datetime
+import time
+from bs4 import BeautifulSoup
+import ftfy
+#You don't need all these imports for this file, just was lazy ;p
+
+def setup(bot): 
+    bot.add_cog(Core(bot))
